8211e8d00fd9a204074fb7d5674919ec1485bd53
root <ansigroup@gmail.com>
2020-04-11 00:09:37 +0000

    add cfund.proton tests

--- contracts/cfund.proton/test.sh
+++ contracts/cfund.proton/test.sh
@@ -1,12 +1,19 @@
 
+### Deploy
+#./cleos.sh set account permission cfund.proton active --add-code -p cfund.proton@active
+#./cleos.sh set contract cfund.proton ./cfund.proton -p cfund.proton
+
+### Admins actions
+#./cleos.sh push action cfund.proton reg '{"account":"cryptolions"}' -p cfund.proton@active
+#./cleos.sh push action cfund.proton unreg '{"account":"cryptolions"}' -p cfund.proton@active
+#./cleos.sh push action cfund.proton activate '{"account":"cryptolions", "status": 0}' -p cfund.proton@active
+#./cleos.sh push action cfund.proton activate '{"account":"cryptolions", "status": 1}' -p cfund.proton@active
+#./cleos.sh push action cfund.proton process '{}' -p cfund.proton@active
+
+### Users Actions
+#./cleos.sh push action cfund.proton claimreward '{"account":"cryptolions"}' -p cryptolions@active
 
 
-
-#./cleos.sh push action cfund.proton remove '{"id": 1000001}' -p eosio.token
-
-#./cleos.sh get table cfund.proton cfund.proton tabl
-
-
-
-
-
+### Tables
+#./cleos.sh get table cfund.proton cfund.proton users
+#./cleos.sh get table cfund.proton cfund.proton global
