2266d1cdca051f8ba29e104bb307fac63030ffc4
root <ansigroup@gmail.com>
2020-12-03 17:15:51 +0000

    add memochecker contract

---
+++ contracts/memochecker/src/memochecker.cpp
@@ -0,0 +1,64 @@
+#include <memochecker.hpp>
+
+optional<uint64_t> memochecker::get_memo_id(const string& memo)
+{
+	optional<uint64_t> result = nullopt;
+
+	auto index_hash = memoholders_.get_index<"hash"_n>();
+	auto it = index_hash.find(make_hash(memo));
+
+	if (it != index_hash.end())
+	{
+		result = optional<uint64_t>(it->id);
+	}
+
+	return result;
+}
+
+ACTION memochecker::addmemo(const string& memo) {
+	require_auth(get_self());
+
+	auto id = get_memo_id(memo);
+	check(!(id.has_value() == true), "memo already exist at record id: " + to_string((id.has_value() == true)?id.value():0));
+	
+	memoholders_.emplace(_self, [&](auto& s) {
+		s.id = memoholders_.available_primary_key();
+		s.set_memo(memo);
+	});
+}
+
+ACTION memochecker::delid(uint64_t id) {
+	require_auth(get_self());
+
+	auto it = memoholders_.require_find(id, string("id " +  to_string(id) + " does not exist").c_str());
+	memoholders_.erase(it);
+}
+
+checksum256 memochecker::make_hash(const string& memo)
+{
+	return sha256(memo.c_str(), memo.size());
+}
+
+ACTION memochecker::delmemo(const string& memo) {
+	require_auth(get_self());
+
+	auto id = get_memo_id(memo);
+	check(!(id.has_value() == false), "memo does not exist at memoholder table");
+
+	auto it = memoholders_.require_find(id.value(), string("id " + to_string(id.value()) + " does not exist").c_str());
+	memoholders_.erase(it);
+}
+
+void memochecker::ontransfer(name from, name to, asset quantity, string memo)
+{
+	if (to != get_self() || from == get_self()) {
+		return;
+	}
+
+	require_auth(from);
+
+	//const auto token_contract = get_first_receiver();
+
+	auto id = get_memo_id(memo);
+	check(!(id.has_value() == false), "memo does not exist at memoholder table");
+}
---
+++ contracts/memochecker/test.sh
@@ -0,0 +1,36 @@
+## Contract which reject transfers with not whitelisted memos.
+## Allowed memos are stored in contract table so can be added and removed.
+
+## Usage
+##  - stake.proton
+
+##
+
+
+##-----------------------------------------------
+
+### Deploy
+#./cleos.sh push action eosio.proton dappreg '["stake.proton"]' -p stake.proton
+#./cleos.sh set contract stake.proton ./build/memochecker -p stake.proton
+
+### Add memo
+#./cleos.sh push action stake.proton addmemo '{"memo":"Stake"}' -p stake.proton
+#./cleos.sh push action stake.proton addmemo '{"memo":"Unstake"}' -p stake.proton
+#./cleos.sh push action stake.proton addmemo '{"memo":"stake bandwidth"}' -p stake.proton
+#./cleos.sh push action stake.proton addmemo '{"memo":"unstake XPR"}' -p stake.proton
+
+
+### Del memo
+#./cleos.sh push action stake.proton delmemo '{"memo":"Stake"}' -p stake.proton
+#./cleos.sh push action stake.proton delmemo '{"memo":"Unstake"}' -p stake.proton
+#./cleos.sh push action stake.proton delmemo '{"memo":"stake bandwidth"}' -p stake.proton
+#./cleos.sh push action stake.proton delmemo '{"memo":"unstake XPR"}' -p stake.proton
+
+
+### Del by ID
+#./cleos.sh push action stake.proton delid '{"id":1}' -p stake.proton
+
+
+
+
+
